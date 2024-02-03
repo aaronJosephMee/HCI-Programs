d164f57fe493d82cdf36a22c7d640f2416cda63b
aaronJosephMee <aaronmee96@gmail.com>
2024-02-02 22:49:51 -0600

    Implemented Part 2.2: Better zooming technique (not fully functional)
    Implemented Part 2.3: Basic magnifying-lens technique
    
    Implemented Part 2.4: Better magnifying-lens technique

--- src/a3.java
+++ src/a3.java
@@ -1,4 +1,7 @@
 import processing.core.PApplet;
+import processing.core.PGraphics;
+
+import java.util.ArrayList;
 
 public class a3 extends PApplet {
     // TODO COPY FROM LINE BELOW ----------------------------------------------------------------------
@@ -38,6 +41,15 @@ public class a3 extends PApplet {
     int errors;
 
     float zoomLevel = 1;
+    boolean displayZoomField = false;
+
+    float zoomFieldX;
+    float zoomFieldY;
+    float zoomFieldWidth = 100;
+    float zoomFieldHeight = 100;
+
+    boolean displayMagnification = false;
+    boolean transparentMagnification = false;
 
     public void settings() {
         size(1500, 900);
@@ -79,6 +91,14 @@ public class a3 extends PApplet {
     public void draw() {
         background(255);
         drawTargets();
+
+        if (displayZoomField) {
+            drawZoomField();
+        }
+
+        if (displayMagnification) {
+            drawMagnifier(mouseX, mouseY);
+        }
     }
 
     public void drawTargets() {
@@ -89,6 +109,8 @@ public class a3 extends PApplet {
             else {
                 fill(150);
             }
+            stroke(105);
+            strokeWeight(1);
             ellipse(targets[i].x, targets[i].y, targets[i].diameter * zoomLevel, targets[i].diameter * zoomLevel);
 
             // TODO - DELETE CONTENTS BELOW AFTER DEBUGGING
@@ -99,6 +121,15 @@ public class a3 extends PApplet {
             text(i + 1, targets[i].x, targets[i].y);
             // TODO - DELETE CONTENTS ABOVE AFTER DEBUGGING
         }
+    }
+
+    public void drawZoomField() {
+        noFill();
+        stroke(255, 0, 0);
+        strokeWeight(2);
+        zoomFieldX = mouseX - (zoomFieldWidth / 2);
+        zoomFieldY = mouseY - ((zoomFieldHeight / 2));
+        rect(zoomFieldX, zoomFieldY, zoomFieldWidth, zoomFieldHeight);
     }
 
     public void keyPressed() {
@@ -113,6 +144,69 @@ public class a3 extends PApplet {
         else if (key == '1' && zoomLevel == 6) {
             zoomOut();
         }
+        if (key == '2' && zoomLevel == 1) {
+            displayZoomField = true;
+        }
+        if (key == '2' && zoomLevel == 6) {
+            zoomOut();
+        }
+        if (key == '3' && zoomLevel == 1 && !displayMagnification) {
+            displayMagnification = true;
+        }
+        else if (key == '3' && displayMagnification) {
+            displayMagnification = false;
+        }
+        if (key == '4' && zoomLevel == 1 && !displayMagnification) {
+            displayMagnification = true;
+            transparentMagnification = true;
+        }
+        else if (key == '4' && displayMagnification) {
+            displayMagnification = false;
+            transparentMagnification = false;
+        }
+    }
+
+    public void keyReleased() {
+        if (key == '2' && displayZoomField) {
+            displayZoomField = false;
+            zoomIn();
+        }
+    }
+
+    public void drawMagnifier(int mouseX, int mouseY) {
+        int magnifySize = 200;
+        int zoom = 6;
+        int magnifyX = constrain(mouseX - magnifySize / 2, 0, width - magnifySize);
+        int magnifyY = constrain(mouseY - magnifySize / 2, 0, height - magnifySize);
+
+        // Draw the magnifying lens
+        if (transparentMagnification) {
+            fill(200, 200, 255, 150);
+        }
+        else {
+            fill(255);
+        }
+        rect(magnifyX, magnifyY, magnifySize, magnifySize);
+
+        // Check if the mouse is over the magnifying lens
+        if (mouseX > magnifyX && mouseX < magnifyX + magnifySize && mouseY > magnifyY && mouseY < magnifyY + magnifySize) {
+            // Find the circle under the magnifying lens
+            for (int i = 0; i < numTargets; i++) {
+                // Check if the mouse is over the circle in the magnifying lens
+                if (dist(targets[i].x, targets[i].y, mouseX, mouseY) < (float) 20 / 2) {
+                    // Draw the magnified circle
+                    if (targets[i].highlighted) {
+                        fill(255, 255, 0);
+                    }
+                    else {
+                        fill(150);
+                    }
+                    ellipse(targets[i].x, targets[i].y, targets[i].diameter * zoom, targets[i].diameter * zoom);
+                    textAlign(CENTER, CENTER);
+                    text(i + 1, targets[i].x, targets[i].y);
+                }
+            }
+        }
     }
 
     public void zoomIn() {
@@ -126,10 +220,35 @@ public class a3 extends PApplet {
     }
 
     public void drawZoomedInCircles(float zoomX, float zoomY) {
+        ArrayList<Target> targetsWithinZoomField = new ArrayList<>();
+
         for (int i = 0; i < numTargets; i++) {
-            targets[i].x = (targets[i].x - zoomX) * zoomLevel + zoomX;
-            targets[i].y = (targets[i].y - zoomY) * zoomLevel + zoomY;
+            if (isTargetWithinZoomField(targets[i].x, targets[i].y)) {
+                targetsWithinZoomField.add(targets[i]);
+            }
+            targets[i].x = ((targets[i].x - zoomX) * zoomLevel) + zoomX;
+            targets[i].y = ((targets[i].y - zoomY) * zoomLevel) + zoomY;
         }
+        println(targetsWithinZoomField.size() + " were found");
+
+//        if (!targetsWithinZoomField.isEmpty()) {
+//            for (int i = 0; i < targetsWithinZoomField.size(); i++) {
+//                float targetX = targetsWithinZoomField.get(i).x;
+//                float targetY = targetsWithinZoomField.get(i).y;
+//
+//                targetsWithinZoomField.get(i).x = ((targetX - mouseX) * zoomLevel) + mouseX;
+//                targetsWithinZoomField.get(i).y = ((targetY - mouseY) * zoomLevel) + mouseY;
+//
+//                ellipse(targetsWithinZoomField.get(i).x, targetsWithinZoomField.get(i).y,
+//                        targetsWithinZoomField.get(i).diameter,
+//                        targetsWithinZoomField.get(i).diameter);
+//            }
+//        }
+    }
+
+    public boolean isTargetWithinZoomField(float targetXCoord, float targetYCoord) {
+        return (targetXCoord > zoomFieldX && targetXCoord < zoomFieldX + zoomFieldWidth
+                && targetYCoord > zoomFieldY && targetYCoord < zoomFieldY + zoomFieldHeight);
     }
 
     public void restoreCanvas() {
